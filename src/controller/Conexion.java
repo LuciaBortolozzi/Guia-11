dcf4e7aeaf2cd54b4bfbbebba70d1470f4a2d915
LuciaBortolozzi <lucia.bortolozzi@usal.edu.ar>
2020-09-02 13:05:22 -0300

    Clase Conexion

---
+++ src/controller/Conexion.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static String server;
+    private static String user;
+    private static String password;
+
+    static {
+        try {
+            Class.forName(driver);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+        boolean lu = true;
+
+        if (lu) {
+            server = "jdbc:sqlserver://DESKTOP-LU;databaseName=Programacion_Avanzada";
+            user = "sa";
+            password = "prog.av";
+        }
+
+        return DriverManager.getConnection(server, user, password);
+    }
+}
